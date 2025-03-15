a88df0a7b3a75acb3079035e34376b0e5d28c70c
lambert.wu <lambertint9527@gmail.com>
2025-03-15 13:33:54 +0800

    Add typedef for signed and unsigned integer type

--- include/common.h
+++ include/common.h
@@ -1,3 +1,15 @@
 #pragma once
 
+#include <stdint.h>
+
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
